9efa7f74cd5adba36b866b57c76b16845f6e7f42
Shailendra Chauhan <pro.shailendra@gmail.com>
2023-05-14 09:06:43 +0530

    Add project files.

---
+++ WebAppFoundations14May/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppFoundations14May.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
